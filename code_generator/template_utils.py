1d89780bed4beed3220a4683962029aff4641852
dazingwill <spumirant@aliyun.com>
2018-03-20 23:45:05 -0700

    code generator code structure adjustment

--- code_generator/template_utils.py
+++ code_generator/template_utils.py
@@ -1,30 +1,46 @@
 import re
 
 TEMPLATE_PREFIX = "$"
+TEMPLATE_FUNC_MARK = "\$FUNC"
+TEMPLATE_FUNC_END_MARK = "\$ENDFUNC"
+
+TEMPLATE_LANGUAGES = ["JavaScript", "Java"]
+
 TEMPLATE_PATH = {"Java": "code_templates/java/",
                  "JavaScript": "code_templates/javascript/"}
 LANGUAGE_SUFFIX = {"Java": ".java",
                    "JavaScript": ".js"}
 
-TEMPLATE_FUNC_MARK = "\$FUNC"
-TEMPLATE_FUNC_END_MARK = "\$ENDFUNC"
+# not used yet
+JAVA_TYPE_MAP = {"Integer": "int"}
+JAVASCRIPT_TYPE_MAP = {}
+LANGUAGE_TYPE_MAP = {"Java": JAVA_TYPE_MAP,
+                     "JavaScript": JAVASCRIPT_TYPE_MAP}
 
 
+def template_output_path(dm_name, language, username = "default"):
+    return "generated_code/%s/%s/%s/" % (username, dm_name, language)
+
+
+# XXX need simplify
 def get_examples(func_name, attribute_list):
     if len(attribute_list) == 0:
-        return None
-    examples = None
-    attr1_name = attribute_list[0]["name"]
-    attr1_type = attribute_list[0]["type"]
+        return []
+    examples = []
+    attr1_name = attribute_list[0]["name"].encode("utf-8")
+    attr1_type = attribute_list[0]["type"].encode("utf-8")
     if func_name == "create":
         example_one = {attr1_name: "some value (" + attr1_type + ")"}
         example_many = [{attr1_name: "some value (" + attr1_type + ")"},
                         {attr1_name: "some other value (" + attr1_type + ")"}]
         examples = [str(example_one), str(example_many)]
-    elif func_name == "read":
+    elif func_name == "read" or func_name == "readOne":
         example_id = {"_id": "specific id (String)"}
         example_attr = {attr1_name: "some value (" + attr1_type + ")"}
         examples = [str(example_id), str(example_attr)]
+    elif func_name == "readMany":
+        example_attr = {attr1_name: "some value (" + attr1_type + ")"}
+        examples = [str(example_attr)]
     elif func_name == "update":
         example_update1 = {"oldData": {"_id": "specific id (String)"},
                            "newData": {attr1_name: "some value (" + attr1_type + ")"}}
@@ -35,7 +51,6 @@ def get_examples(func_name, attribute_list):
         example_id = {"_id": "specific id (String)"}
         example_attr = {attr1_name: "some value (" + attr1_type + ")"}
         examples = [str(example_id), str(example_attr)]
-
     return examples
 
 
@@ -84,6 +99,8 @@ def replace_words(template_content, replacements):
     return content
 
 
+
+
 # for test
 '''def generate_template(ori_filename, simple_variables, replacements, output_filename):
     file_content = open(ori_filename, "r").read()
@@ -95,3 +112,19 @@ def replace_words(template_content, replacements):
     output_file = open(output_filename, "w")
     output_file.write(file_content)
     output_file.close()'''
+
+'''
+def extract_model_data(json_data):
+    model_data = {}
+    for model_name in json_data:
+        for element in json_data.get(model_name).get("elements"):
+            elem_name = element.get("elementName")
+            element_attrs = element.get("Attributes").get("Simple")
+            attribute_list = [{"name": attribute["name"],
+                               "type": attribute["details"]["type"]}
+                              for attribute in element_attrs]
+            model_data[elem_name] = attribute_list
+        #for operation in json_data.get(model_name).get("Operations"):
+            #operation_name = operation["name"]
+    return model_data
+'''
