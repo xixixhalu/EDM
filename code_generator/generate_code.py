b7ed58356340e7e5e06bc4d89ab72efdde9acaa6
Matt Stephens <matt@softwarereality.com>
2019-01-17 11:44:36 +0000

    Allow code (esp Java) to be placed in a sub-path to bring it in line with standard maven-like project structures; also add support for namespaces in the generated code.

--- code_generator/generate_code.py
+++ code_generator/generate_code.py
@@ -1,6 +1,6 @@
 import sys
+
 sys.path.append('../')
-import json
 from config import config
 from utilities.config_util import ConfigUtil
 from utilities import port_scanner, edm_utils
@@ -10,19 +10,19 @@ from utilities.file_op import fileOps
 
 from uml_parser.JSONParser import JSONParser
 from uml_parser.UMLViewer import *
-
 from code_generator.ApiGenerator import ApiGenerator
+
 
 def get_server_info():
     # display_ip = "34.223.218.62" # Local env 127.0.0.0 Prev 18.216.141.169
     # server_ip = "0.0.0.0" # Local env - 127.0.0.0
     # port = port_scanner.runPortScan(2000, 6000)
 
-    config = ConfigUtil()
-    display_ip = config.get('IP', 'display_ip')
-    server_ip = config.get('IP', 'server_ip')
-    from_port = config.getInt('Port', 'from_port')
-    to_port = config.getInt('Port', 'to_port')
+    config_util = ConfigUtil()
+    display_ip = config_util.get('IP', 'display_ip')
+    server_ip = config_util.get('IP', 'server_ip')
+    from_port = config_util.getInt('Port', 'from_port')
+    to_port = config_util.getInt('Port', 'to_port')
 
     port = port_scanner.runPortScan(from_port, to_port)
     return str(display_ip), str(server_ip), str(port)
@@ -30,7 +30,7 @@ def get_server_info():
 
 def configure_db(db_name):
     dbutils = DBUtilities()
-    dbutils.setup(configDictionary={"host":"127.0.0.1","port":27017})
+    dbutils.setup(configDictionary={"host": "127.0.0.1", "port": 27017})
     dbutils.createWithUser(db_name)
 
 
@@ -39,7 +39,7 @@ def generate_model(language, template_model, output_dir, to_file):
     Creates the Model code file with a specific language
     :param language: the language name. eg: 'JavaScript'
     :param template_model: template model for a class, see class TemplateModel
-    :param output_dir: the output path
+    :param output_dir: the root output path
     :param to_file: whether to rewrite the file
     :return: data used for code display in web page
     """
@@ -52,6 +52,7 @@ def generate_model(language, template_model, output_dir, to_file):
 
     code_display_data = template.get_display_data()
     return code_display_data
+
 
 def generate_adapter(language, server_ip, port, dm_name, output_dir, to_file):
     """
@@ -68,7 +69,7 @@ def generate_adapter(language, server_ip, port, dm_name, output_dir, to_file):
 
     data = {"server_ip": server_ip,
             "port": port,
-            "dm_name" : dm_name}
+            "dm_name": dm_name}
     template.render(tofile=to_file, reset=False, replace_words=data)
 
     code_display_data = template.get_display_data()
@@ -76,6 +77,8 @@ def generate_adapter(language, server_ip, port, dm_name, output_dir, to_file):
 
 
 """ This method creates the server file"""
+
+
 def generate_server(server_ip, port, output_path, dm_name, json_data):
     """
     Creates the server code file for the REST services
@@ -86,20 +89,20 @@ def generate_server(server_ip, port, output_path, dm_name, json_data):
     :json_data: the domain model json structure
     """
     server_file = open("code_templates/" + "Server", "r")
-    class_file = open("code_templates/"+ "class_template", "r")
+    class_file = open("code_templates/" + "class_template", "r")
 
     db_template_path = config.get('Output', 'instance_db_template') + "/"
-    db_schema_file = open(db_template_path+ "db_schema_template", "r")
-    db_schema_array_file = open(db_template_path+ "db_schema_array_template", "r")
-    db_schema_nested_file = open(db_template_path+ "db_schema_nested_template", "r")
-    db_schema_nested_array_file = open(db_template_path+ "db_schema_nested_array_template", "r")
-    db_connection_file = open(db_template_path+ "db_connection_template", "r")
-    db_ops_file = open(db_template_path+ "db_ops_template", "r")
-    db_schema_validation_file = open(db_template_path+ "db_schema_validation_template", "r")
-    authen_file = open("code_templates/"+ "authen_template", "r")
-    behavior_file = open("code_templates/"+ "behavior", "r")
-    package_json_file = open("code_templates/"+ "package.json", "r")
-    type_converter_file = open(db_template_path+ "typeConverter", "r")
+    db_schema_file = open(db_template_path + "db_schema_template", "r")
+    db_schema_array_file = open(db_template_path + "db_schema_array_template", "r")
+    db_schema_nested_file = open(db_template_path + "db_schema_nested_template", "r")
+    db_schema_nested_array_file = open(db_template_path + "db_schema_nested_array_template", "r")
+    db_connection_file = open(db_template_path + "db_connection_template", "r")
+    db_ops_file = open(db_template_path + "db_ops_template", "r")
+    db_schema_validation_file = open(db_template_path + "db_schema_validation_template", "r")
+    authen_file = open("code_templates/" + "authen_template", "r")
+    behavior_file = open("code_templates/" + "behavior", "r")
+    package_json_file = open("code_templates/" + "package.json", "r")
+    type_converter_file = open(db_template_path + "typeConverter", "r")
 
     server_template = server_file.read()
     class_template = class_file.read()
@@ -122,7 +125,7 @@ def generate_server(server_ip, port, output_path, dm_name, json_data):
     log_path = output_path + "/log/"
     if not os.path.isdir(log_path):
         os.makedirs(log_path)
-    
+
     elements = None
     for model_name in json_data:
         elements = json_data[model_name].get("elements")
@@ -131,12 +134,12 @@ def generate_server(server_ip, port, output_path, dm_name, json_data):
     data = {"server_ip": server_ip,
             "port": port,
             "db_name": dm_name,
-            "collection_names" : str(elem_names)}
+            "collection_names": str(elem_names)}
     content = replace_words(server_template, data)
     server_code = fileOps.safe_open_w(output_path + "Server" + ".js")
     server_code.write(content)
     server_code.close()
-    
+
     package_json = fileOps.safe_open_w(output_path + "package" + ".json")
     package_json.write(package_json_template)
     package_json.close()
@@ -188,13 +191,14 @@ def generate_server(server_ip, port, output_path, dm_name, json_data):
                     content = replace_words(db_schema_nested_array_template, data)
                 else:
                     content = replace_words(db_schema_nested_template, data)
-                    
+
                 attribute_schema.append(content)
         except Exception as e:
             print "In standard parser, the complex attributes are not handled"
             pass
+
     # End for handling nested object
-    
+
     for entity_id, entity_name in jp.entities().items():
         attribute_names = set()
         attribute_schema = []
@@ -203,30 +207,27 @@ def generate_server(server_ip, port, output_path, dm_name, json_data):
         add_complex_attribute_schema(entity_name, attribute_names, attribute_schema, jp)
 
         data = {
-            "collection_name" : entity_name
+            "collection_name": entity_name
         }
         validator = replace_words(db_schema_validation_template, data)
 
         data = {
-            "collection_name" : entity_name,
-            "attribute_names" : str(list(attribute_names)),
-            "attribute_schema" : ',\n'.join(attribute_schema),
-            "validator" : validator
+            "collection_name": entity_name,
+            "attribute_names": str(list(attribute_names)),
+            "attribute_schema": ',\n'.join(attribute_schema),
+            "validator": validator
         }
         content = replace_words(class_template, data)
         output_location = output_path + entity_name + ".js"
         with fileOps.safe_open_w(output_location) as output_file:
             output_file.write(content)
 
-
-
     # generate behaviors
-    behaviors = [element["Behaviors"] for element in elements]
-    for element in elements :
+    for element in elements:
         for behavior in element["Behaviors"]:
             output_location = output_path + "Behaviors/" + str(behavior['name']) + ".js"
-            data = { "entity" : str(element["elementName"]),
-                     "behavior": str(behavior['name'])
+            data = {"entity": str(element["elementName"]),
+                    "behavior": str(behavior['name'])
                     }
             content = replace_words(behavior_template, data)
             with fileOps.safe_open_w(output_location) as output_file:
@@ -236,9 +237,9 @@ def generate_server(server_ip, port, output_path, dm_name, json_data):
     output_location = output_path + "db_connection" + ".js"
     configure_db(dm_name)
     db_user, db_password = edm_utils.generate_user_credentials(dm_name)
-    data = {"db_user": db_user, 
+    data = {"db_user": db_user,
             "db_password": db_password,
-            "db_name" : dm_name}
+            "db_name": dm_name}
     with fileOps.safe_open_w(output_location) as output_file:
         content = replace_words(db_connection_template, data)
         output_file.write(content)
@@ -273,6 +274,8 @@ def generate_server(server_ip, port, output_path, dm_name, json_data):
 
 
 """ This method creates the api reference page """
+
+
 def generate_api_reference(server_ip, port, output_path, dm_name, json_data):
     gen = ApiGenerator()
     gen.set_basic_path(server_ip + ":" + port, dm_name)
@@ -283,7 +286,7 @@ def generate_api_reference(server_ip, port, output_path, dm_name, json_data):
 
     for entity_id, entity_name in jp.entities().items():
         gen.add_entity(entity_name, jp)
-    
+
     gen.generate_file(output_path)
 
 
@@ -325,23 +328,24 @@ def generate_all(dm_name, output_dir, to_file=True):
     file_location = output_dir + "/" + dm_name + ".json"
     with open(file_location) as json_input:
         json_data = json.load(json_input)
-        #print json.dumps(json_data, indent=2)
+        # print json.dumps(json_data, indent=2)
         display_ip, server_ip, port = get_server_info()
         print(display_ip + ":" + port)
 
         model_display_data = {}
 
         # generate adapter code files
-        model_display_data["Adapter"] = {language: generate_adapter(language, server_ip, port, dm_name, output_dir, to_file)
-                                         for language in TEMPLATE_LANGUAGES}
+        model_display_data["Adapter"] = {
+            language: generate_adapter(language, server_ip, port, dm_name, output_dir, to_file)
+            for language in TEMPLATE_LANGUAGES}
 
         # read all the class models from json_data
         template_models = TemplateModel.extract_models(json_data)
         # generate class model code files
         for template_model in template_models:
-            model_display_data[template_model.name] = {language: generate_model(language, template_model, output_dir, to_file)
-                                                       for language in TEMPLATE_LANGUAGES}
-
+            model_display_data[template_model.name] = {
+                language: generate_model(language, template_model, output_dir, to_file)
+                for language in TEMPLATE_LANGUAGES}
 
         # generate server code files
         generate_server(str(server_ip), str(port), output_dir, dm_name, json_data)
@@ -351,11 +355,12 @@ def generate_all(dm_name, output_dir, to_file=True):
         # generate UML diagram
         generate_diagram(json_data, dm_name, output_dir)
 
-        #TODO
-        #write model_display_data and display_ip to file
+        # TODO
+        # write model_display_data and display_ip to file
 
         return model_display_data, display_ip + ":" + str(port)
         # return model_display_data
+
 
 def write_description_to_file(dm_name, output_dir, meta_data):
     file_location = output_dir + "/" + dm_name + "Modeldata" + ".json"
@@ -363,7 +368,8 @@ def write_description_to_file(dm_name, output_dir, meta_data):
     with fileOps.safe_open_w(file_location) as json_input:
         json_input.write(md)
     json_input.close()
-  
+
+
 def read_description_from_file(dm_name, file_path):
     file_location = file_path + "/" + dm_name + "Modeldata" + ".json"
     with open(file_location, 'r') as json_input:
